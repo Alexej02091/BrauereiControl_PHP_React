010309f79f5007738c4651f0f7f5c35ad9105185
Alexej0209 <alexeyalferov1988@gmail.com>
2026-01-23 18:50:56 +0100

    Add template for header

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ npm create vite braurei -- --template react
 ```
 npm install react-bootstrap bootstrap
 ```
+```
+App.jsx
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+```
 
 # Projekt staten
 ```
@@ -16,5 +21,9 @@ npm run dev
 ```
 
 # Quellen
-**React Sebastian Springer** ISBN 978-3-8362-9254-2
-- Projekt initialisieren ***S. 63***
+## **React Sebastian Springer** ISBN 978-3-8362-9254-2
+- 2.4.3 Projekt initialisieren ***S. 63***
+- 3.3.1 Eine Komponente pro Datei ***S. 63***
+
+## **[React Bootstrap](https://react-bootstrap.netlify.app/)**
+- [Introduction](https://react-bootstrap.netlify.app/docs/getting-started/introduction)
---
+++ brauerei/src/components/header/Header.jsx
@@ -0,0 +1,41 @@
+import {Nav, Container, Row, Col, Button} from 'react-bootstrap';
+
+function Header() {
+
+    return(
+        <Container>
+          <Row className="align-items-center py-2">
+            <Col>
+              <h3 style={{ margin: 0 }}>Irtysch GmbH</h3>
+            </Col>
+            <Col className="text-end">
+              <Button variant="primary" className="me-2">Login</Button>
+            </Col>
+          </Row>
+        
+
+     
+     <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Produnk</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Kontakt</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+    </Container>
+    );
+}
+
+export default Header
