9487513d3a121269a4b43a36dfb2af0756dff37e
Alexej0209 <alexeyalferov1988@gmail.com>
2026-03-23 17:52:14 +0100

    Clean Backend

--- brauerei/backend/db/mock_db/mock_datenbank_drop.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(con, datenbankName, callback) {
-    con.query(`DROP DATABASE ${datenbankName}`, 
-        function (err_drop) {
-            if (err_drop) return callback(err_drop);
-                console.log(`Database ${datenbankName} was dropped`);
-                callback(null);
-        }
-    );
-}
